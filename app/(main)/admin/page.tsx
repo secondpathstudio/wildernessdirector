053d5c487f904a5412e2a8e2fb8f9bcf4e932678
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-04-23 16:17:35 -0700

    removed toaster from admin

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -1,11 +1,9 @@
 import { AdminContainer } from "@/components/admin/admin-container";
-import { Toaster } from "@/components/ui/toaster";
 
 const AdminPage = () => {
   return (
     <>
       <AdminContainer />
-      <Toaster />
     </>
   );
 };
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,3 @@
-import { Badge } from "@/components/ui/badge";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 import React from "react";
 
 export default function Home() {
