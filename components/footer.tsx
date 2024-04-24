205d294d65aee37dfa680ae84578491bbc4c490f
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-04-24 13:50:53 -0700

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,23 @@
 import WildermedIcon from "../components/ui/WildermedIcon"
 
 export const Footer = () => {
+
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
     <footer>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left text-[#283618]">
-            A{" "}
+            ©️ {year}{" "}
             <a
               href="https://www.cvem.us"
               target="_blank"
               className="font-medium underline underline-offset-4"
             >
               Coachella Valley Emergency Medicine Ultrasound
-            </a>{" "}
-            project.
+            </a>
           </p>
         </div>
       </div>
