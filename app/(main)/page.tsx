a5add2276c566c66aeb69978b5368f7feb642912
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-05-08 14:05:06 -0700

    adjusted footer and centered text

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="grow flex flex-col items-center justify-center">
-        Innovating Wilderness Medicine in Southern California
+      <div className="grow flex flex-col items-center justify-center text-center">
+        <h2>Innovating Wilderness Medicine in Southern California</h2>
         <div className="bg-secondary text-background rounded-lg px-6 py-3 mt-6 -rotate-6">
           Coming Soon!
         </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -8,8 +8,8 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose md:text-left text-[#283618]">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2 md:px-0">
+          <p className="text-center md:text-sm text-xs leading-loose md:text-left text-[#283618]">
             ©️ {year}{" "}
             <a
               href="https://www.cvem.us"
