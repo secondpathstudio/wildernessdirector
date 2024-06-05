129ea7bba8e162b9c4dc7bfd9695b815288b813d
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-04 21:03:22 -0700

    show objectives completed count on topic overview

--- components/topic-overview/topic-overview.tsx
+++ components/topic-overview/topic-overview.tsx
@@ -70,10 +70,10 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
     <>
         <div className="flex-1 space-y-4 pt-6">
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
+          <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  Questions Submitted
+                  Objectives Completed
                 </CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -85,22 +85,20 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
                   strokeWidth="2"
                   className="h-4 w-4 text-muted-foreground"
                 >
-                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                  <circle cx="9" cy="7" r="4" />
+                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
                 </svg>
               </CardHeader>
               <CardContent>
-                {questionStatus === "loading" && (
-                  <div className="text-2xl font-bold">...</div>
-                )}
-                {questionStatus === "error" && (
-                  <div className="text-2xl font-bold">!</div>
-                )}
-                {questionStatus === "success" && (
-                  <div className="text-2xl font-bold">{questions.length}</div>
-                )}
+                {props.topicData?.userProgress?.length > 0 ? (
+                  <div className="text-2xl font-bold">{props.topicData.userProgress.find((u: any) => (u.userId === auth.currentUser?.uid)).completedObjectives}</div>
+                ) :
+                  <div className="text-2xl font-bold">0</div>
+                }
               </CardContent>
             </Card>
-            <Card>
+          <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
                   Field Reports
@@ -132,6 +130,37 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
                 )}
               </CardContent>
             </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Questions Submitted
+                </CardTitle>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="h-4 w-4 text-muted-foreground"
+                >
+                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                </svg>
+              </CardHeader>
+              <CardContent>
+                {questionStatus === "loading" && (
+                  <div className="text-2xl font-bold">...</div>
+                )}
+                {questionStatus === "error" && (
+                  <div className="text-2xl font-bold">!</div>
+                )}
+                {questionStatus === "success" && (
+                  <div className="text-2xl font-bold">{questions.length}</div>
+                )}
+              </CardContent>
+            </Card>
+            
           </div>
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
             <Card className="col-span-4">
