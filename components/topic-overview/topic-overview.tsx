b2c06c745e20b7cbb435c5182bc3fb7ba1b34cf5
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-04 21:06:19 -0700

    better icons for overview

--- components/topic-overview/topic-overview.tsx
+++ components/topic-overview/topic-overview.tsx
@@ -11,6 +11,7 @@ import {
 import { useAuth, useFirestore, useFirestoreCollectionData } from "reactfire";
 import { collection, query, where } from "firebase/firestore";
 import ScheduleCalendar from "../ui/schedule-calendar";
+import { BookOpen, FileQuestion, ListChecks } from "lucide-react";
 
 interface TopicOverviewProps {
   topicId: string;
@@ -75,20 +76,7 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
                 <CardTitle className="text-sm font-medium">
                   Objectives Completed
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
+                <ListChecks />
               </CardHeader>
               <CardContent>
                 {props.topicData?.userProgress?.length > 0 ? (
@@ -103,20 +91,7 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
                 <CardTitle className="text-sm font-medium">
                   Field Reports
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
+                <BookOpen />
               </CardHeader>
               <CardContent>
                 {fieldReportStatus === "loading" && (
@@ -135,18 +110,7 @@ export const TopicOverview: FC<TopicOverviewProps> = (props) => {
                 <CardTitle className="text-sm font-medium">
                   Questions Submitted
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                </svg>
+                <FileQuestion />
               </CardHeader>
               <CardContent>
                 {questionStatus === "loading" && (
