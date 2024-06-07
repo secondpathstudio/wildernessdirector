9485e40c8045b98dded7442224b921af1787fbd4
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-07 08:47:58 -0700

    use icons on main nav on mobile

--- components/dashboard/main-nav.tsx
+++ components/dashboard/main-nav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { FC } from "react";
+import { BookOpen, FileQuestion, ListChecks } from "lucide-react";
 
 interface TopicNavProps {
   changeTab: (index: number) => void,
@@ -10,34 +11,65 @@ interface TopicNavProps {
 
 export const MainNav: FC<TopicNavProps> = ({ changeTab, ...props }) => {
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6")}
-      {...props}
-    >
-      <button
-        onClick={() => changeTab(0)}
-        className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 0 && 'text-primary'}`}
+    <>
+      <nav
+        className={cn("hidden md:flex items-center space-x-4 lg:space-x-6")}
+        {...props}
       >
-        Overview
-      </button>
-      <button
-        onClick={() => changeTab(1)}
-        className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 1 && 'text-primary'}`}
+        <button
+          onClick={() => changeTab(0)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 0 && 'text-primary'}`}
+        >
+          Overview
+        </button>
+        <button
+          onClick={() => changeTab(1)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 1 && 'text-primary'}`}
+        >
+          Objectives
+        </button>
+        <button
+          onClick={() => changeTab(2)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 2 && 'text-primary'}`}
+        >
+          Field Reports
+        </button>
+        <button
+          onClick={() => changeTab(3)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 3 && 'text-primary'}`}
+        >
+          Questions
+        </button>
+      </nav>
+      <nav
+        className={cn("flex md:hidden items-center w-full justify-between space-x-4 lg:space-x-6")}
+        {...props}
       >
-        Objectives
-      </button>
-      <button
-        onClick={() => changeTab(2)}
-        className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 2 && 'text-primary'}`}
-      >
-        Field Reports
-      </button>
-      <button
-        onClick={() => changeTab(3)}
-        className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 3 && 'text-primary'}`}
-      >
-        Questions
-      </button>
-    </nav>
+        <button
+          onClick={() => changeTab(0)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 0 && 'text-primary'}`}
+        >
+          Overview
+        </button>
+        <button
+          onClick={() => changeTab(1)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 1 && 'text-primary'}`}
+        >
+          <ListChecks />
+        </button>
+        <button
+          onClick={() => changeTab(2)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 2 && 'text-primary'}`}
+        >
+          <BookOpen />
+        </button>
+        <button
+          onClick={() => changeTab(3)}
+          className={`text-sm font-medium transition-colors hover:text-primary ${props.activeTab === 3 && 'text-primary'}`}
+        >
+          <FileQuestion />
+        </button>
+      </nav>
+    </>
   );
 }
