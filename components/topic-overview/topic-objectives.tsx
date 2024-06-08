a8360925811f92e38ebd0e7650ede3e95d390ebd
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-08 09:11:49 -0700

    updated import

--- components/topic-overview/topic-objectives.tsx
+++ components/topic-overview/topic-objectives.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { FC, useState } from "react";
-import { MainNav } from "@/components/dashboard/main-nav";
+import { MainNav } from "@/components/topic-overview/main-nav";
 import {
   Card,
   CardHeader,
