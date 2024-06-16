ce66191a0cce6185f9b57b340f5e34e57187e130
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-16 13:56:07 -0700

    added link to secondpath.dev

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,13 +11,13 @@ export const Footer = () => {
         <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2 md:px-0">
           <p className="text-center md:text-sm text-xs leading-loose md:text-left text-[#283618]">
             ©️ {year}{" "}
-            {/* <a
-              href="https://www.cvem.us"
+            <a
+              href="https://www.secondpath.dev"
               target="_blank"
               className="font-medium underline underline-offset-4"
-            > */}
+            >
               Second Path Studio
-            {/* </a> */}
+            </a>
           </p>
         </div>
       </div>
