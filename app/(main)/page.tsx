46a99e289aa92cc93b80637514e7ff67fabc134f
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2024-06-16 11:36:39 -0700

    hero page animation

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,19 @@
+import HeroPath from "@/components/home/hero-path";
 import React from "react";
 
 export default function Home() {
   return (
     <>
-      <div className="grow flex flex-col items-center justify-center text-center">
-        <h2>Innovating Wilderness Medicine in Southern California</h2>
-        <div className="bg-secondary text-background rounded-lg px-6 py-3 mt-6 -rotate-6">
-          Coming Soon!
+      <section className="grow flex flex-col justify-center items-center">
+        <div className="grid md:grid-cols-2 grid-cols-1 text-5xl font-bold md:tracking-widest md:ml-16 text-secondary md:order-last mx-4">
+          <div className="flex items-center">
+            <h2 className="md:text-right text-center">An innovative approach to Wilderness Medicine fellowship training</h2>
+          </div>
+          <div className="flex items-center justify-center md:justify-start">
+            <HeroPath />
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
---
+++ components/home/hero-path.tsx
@@ -0,0 +1,91 @@
+'use client'
+import React, { useEffect, useRef, useState } from 'react'
+import { DuoButton } from '../ui/button-duo'
+import { cn } from '@/lib/utils'
+import { TreePine } from 'lucide-react'
+import path from 'path'
+
+type Props = {}
+
+const HeroPath = (props: Props) => {
+    const [progress, setProgress] = useState(0);  // Progress as a percentage
+    const pathRef = useRef<SVGPathElement>(null);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setProgress((oldProgress) => {
+          if (oldProgress >= 120) return 0;  // Reset after reaching 100%
+          return oldProgress + 10;  // Increment progress
+        });
+      }, 200);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+    const strokeDasharray = pathRef.current ? pathRef.current.getTotalLength() : 0;
+    const strokeDashoffset = ((100 - progress) / 100) * strokeDasharray;
+  
+
+  return (
+<div style={{ position: 'relative', height: '400px', width: '200px' }}>
+    <DuoButton
+        size="rounded"
+        variant={"secondary"}
+        className="z-20 h-[70px] w-[70px] border-b-8 absolute top-[30px] left-[65px]"
+        >
+            <TreePine
+            className={cn(
+            "h-10 w-10",
+            )}
+            />
+    </DuoButton>
+      <svg style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}>
+        {/* Background Path */}
+        <path
+            className="stroke-neutral-200"
+            d="M 100 70 Q 110 110 100 150"
+            strokeWidth="8"
+            fill="none"
+        />
+        {/* Animated Path */}
+        <path 
+            ref={pathRef}
+            d="M 100 70 Q 110 110 100 150" 
+            stroke={`hsl(${progress} 38% 15%)`} 
+            stroke-width="8" 
+            fill="none"
+            strokeDasharray={strokeDasharray}
+            strokeDashoffset={strokeDashoffset}
+        />
+      </svg>
+      <DuoButton
+        size="rounded"
+        variant={progress >= 100 ? "secondary" : "locked"}
+        className="z-20 h-[70px] w-[70px] border-b-8 absolute top-[140px] left-[60px]"
+        >
+            <TreePine
+            className={cn(
+            "h-10 w-10",
+            )}
+            />
+    </DuoButton>
+      <svg style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}>
+        <path className='stroke-neutral-200' d="M 100 190 Q 90 230 100 270" stroke-width="8" fill="none"/>
+      </svg>
+      <DuoButton
+        size="rounded"
+        variant={"locked"}
+        className="z-20 h-[70px] w-[70px] border-b-8 absolute top-[260px] left-[65px]"
+        >
+            <TreePine
+            className={cn(
+            "h-10 w-10",
+            )}
+            />
+    </DuoButton>
+    </div>
+
+  )
+}
+
+export default HeroPath
